19acb3fbda369ffd6a4e596b2559dacebf131395
leonid <kotovpetia@gmail.com>
2022-11-22 03:10:49 +0300

    TelegramBot added

---
+++ telegrambot-springboot-starter/src/main/java/org/telegram/telegrambot/bot/TelegramBot.java
@@ -0,0 +1,46 @@
+package org.telegram.telegrambot.bot;
+
+import org.telegram.telegrambot.handler.UpdateDispatcher;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final String botUsername;
+    private final String botToken;
+
+    protected final TelegramBotsApi telegramBotsApi;
+    protected final UpdateDispatcher updateDispatcher;
+
+    public TelegramBot(String botUsername, String botToken,
+                       TelegramBotsApi telegramBotsApi, UpdateDispatcher updateDispatcher) {
+        this.botUsername = botUsername;
+        this.botToken = botToken;
+        this.telegramBotsApi = telegramBotsApi;
+        this.updateDispatcher = updateDispatcher;
+    }
+
+    @PostConstruct
+    public void register() throws TelegramApiException {
+        telegramBotsApi.registerBot(this);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        updateDispatcher.executeHandlerOnUpdate(update, this);
+    }
+}
