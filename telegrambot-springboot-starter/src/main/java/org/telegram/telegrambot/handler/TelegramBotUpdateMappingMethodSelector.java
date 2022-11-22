06aaeecb85700e90b3cbdbbbed7cb0e8cfb85e12
leonid <kotovpetia@gmail.com>
2022-11-22 03:09:53 +0300

    UpdateMappingMethodSelector

---
+++ telegrambot-springboot-starter/src/main/java/org/telegram/telegrambot/handler/TelegramBotUpdateMappingMethodSelector.java
@@ -0,0 +1,35 @@
+package org.telegram.telegrambot.handler;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambot.annotation.UpdateMapping;
+import org.telegram.telegrambot.state.StateSource;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+@Component
+public class TelegramBotUpdateMappingMethodSelector implements UpdateMappingMethodSelector {
+
+    private final StateSource stateSource;
+
+    public TelegramBotUpdateMappingMethodSelector(StateSource stateSource) {
+        this.stateSource = stateSource;
+    }
+
+    @Override
+    public Optional<Method> lookupHandlerMappingMethod(Update update, Object handler) {
+        Method[] methods = handler.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            UpdateMapping annotation = method.getAnnotation(UpdateMapping.class);
+            if (annotation != null) {
+                long chatId = update.getMessage().getChatId();
+                String state = stateSource.getState(chatId);
+                if (state.equals(annotation.state())) {
+                    return Optional.of(method);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+}
