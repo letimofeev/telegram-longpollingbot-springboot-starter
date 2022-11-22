75a2dc56aac6a6172e834e4f55863b6e6ab53f80
leonid <kotovpetia@gmail.com>
2022-11-23 00:21:15 +0300

    ExceptionHandlerAnnotationBeanPostProcessor duplicate ExceptionMapping resolving

--- telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/annotation/ExceptionHandlerAnnotationBeanPostProcessor.java
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/annotation/ExceptionHandlerAnnotationBeanPostProcessor.java
@@ -3,11 +3,13 @@ package org.telegram.telegrambot.annotation;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.lang.NonNull;
+import org.telegram.telegrambot.expection.DefaultExceptionHandler;
 import org.telegram.telegrambot.model.ExceptionMappingMethodContainer;
 import org.telegram.telegrambot.model.MethodTargetPair;
 import org.telegram.telegrambot.validator.ExceptionMappingMethodSignatureValidator;
 
 import java.lang.reflect.Method;
+import java.util.Optional;
 
 public class ExceptionHandlerAnnotationBeanPostProcessor implements BeanPostProcessor {
 
@@ -27,9 +29,24 @@ public class ExceptionHandlerAnnotationBeanPostProcessor implements BeanPostProc
             if (annotation != null) {
                 Class<? extends Exception> exceptionType = annotation.value();
                 methodSignatureValidator.validateMethodSignature(method);
-                methodContainer.putExceptionMapping(exceptionType, new MethodTargetPair(method, bean));
+                putWithDuplicatesValidation(exceptionType, method, bean);
             }
         }
         return bean;
+    }
+
+    public void putWithDuplicatesValidation(Class<? extends Exception> exceptionType, Method method, Object target) {
+        Optional<MethodTargetPair> exceptionMappingOptional = methodContainer.getExceptionMapping(exceptionType);
+        if (exceptionMappingOptional.isPresent()) {
+            MethodTargetPair storedExceptionMapping = exceptionMappingOptional.get();
+            if (storedExceptionMapping.getTarget() instanceof DefaultExceptionHandler) {
+                methodContainer.putExceptionMapping(exceptionType, new MethodTargetPair(method, target));
+            } else if (!(target instanceof DefaultExceptionHandler)) {
+                String message = String.format("Found duplicate method annotated as @ExceptionMapping with same exception: %s and %s", method, exceptionType);
+                throw new IllegalStateException(message);
+            }
+        } else {
+            methodContainer.putExceptionMapping(exceptionType, new MethodTargetPair(method, target));
+        }
     }
 }
