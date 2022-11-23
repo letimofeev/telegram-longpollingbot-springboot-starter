3e10401663d610e2dec515038a2488877b58ec80
leonid <kotovpetia@gmail.com>
2022-11-23 17:49:44 +0300

    additional-spring-configuration-metadata.json updated

--- telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/config/StateSourceConfiguration.java
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/config/StateSourceConfiguration.java
@@ -1,5 +1,6 @@
 package org.telegram.telegrambot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,7 @@ public class StateSourceConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public StateSource stateSource() {
-        return new DefaultStateSource("");
+    public StateSource stateSource(@Value("telegrambot.initial-state:") String initialState) {
+        return new DefaultStateSource(initialState);
     }
 }
