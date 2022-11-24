e8c2969b7ed83724834ba0f173b252140550dc21
leonid <kotovpetia@gmail.com>
2022-11-24 13:47:28 +0300

    TelegramBotAutoConfiguration redundant bean removed

--- telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/config/TelegramBotAutoConfiguration.java
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/config/TelegramBotAutoConfiguration.java
@@ -3,7 +3,6 @@ package org.telegram.telegrambot.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +10,7 @@ import org.telegram.telegrambot.bot.DispatchedTelegramLongPollingBot;
 import org.telegram.telegrambot.bot.TelegramLongPollingBotExtended;
 import org.telegram.telegrambot.handler.BotApiMethodExecutorResolver;
 import org.telegram.telegrambot.handler.UpdateResolver;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.starter.TelegramBotStarterConfiguration;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @ComponentScan("org.telegram.telegrambot")
@@ -23,13 +19,6 @@ public class TelegramBotAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
-        return new TelegramBotsApi(DefaultBotSession.class);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    @ConditionalOnProperty("telegrambot.token")
     public TelegramLongPollingBotExtended longPollingBot(@Value("${telegrambot.username}}") String botUsername,
                                                          @Value("${telegrambot.token}") String botToken,
                                                          UpdateResolver updateResolver,
