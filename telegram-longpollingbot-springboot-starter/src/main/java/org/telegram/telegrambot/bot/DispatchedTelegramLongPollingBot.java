accdc58b389239634a8306715b4f47ca68cf1b5b
leonid <kotovpetia@gmail.com>
2022-11-24 21:16:15 +0300

    DispatchedTelegramLongPollingBot logging added

--- telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/bot/DispatchedTelegramLongPollingBot.java
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/bot/DispatchedTelegramLongPollingBot.java
@@ -1,5 +1,7 @@
 package org.telegram.telegrambot.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.ReflectionUtils;
 import org.telegram.telegrambot.handler.BotApiMethodExecutorResolver;
 import org.telegram.telegrambot.handler.UpdateResolver;
@@ -16,6 +18,8 @@ public class DispatchedTelegramLongPollingBot extends TelegramLongPollingBotExte
     private final String botToken;
     private final UpdateResolver updateResolver;
     private final BotApiMethodExecutorResolver methodExecutorResolver;
+
+    private final Logger log = LoggerFactory.getLogger(DispatchedTelegramLongPollingBot.class);
 
     public DispatchedTelegramLongPollingBot(String botUsername, String botToken, UpdateResolver updateResolver, BotApiMethodExecutorResolver methodExecutorResolver) {
         this.botUsername = botUsername;
@@ -36,21 +40,23 @@ public class DispatchedTelegramLongPollingBot extends TelegramLongPollingBotExte
 
     @Override
     public void onUpdateReceived(Update update) {
+        log.debug("Received update: {}", update);
         List<? extends PartialBotApiMethod<Message>> apiMethods = updateResolver.getResponse(update);
         executeAllApiMethods(apiMethods);
     }
 
     @Override
     public void executeApiMethod(PartialBotApiMethod<Message> apiMethod) {
+        log.debug("Executing api method: {}", apiMethod);
         Method apiMethodExecutor = methodExecutorResolver.getApiMethodExecutionMethod(apiMethod);
+        log.debug("Found execute() method: {}", apiMethodExecutor);
         ReflectionUtils.invokeMethod(apiMethodExecutor, this, apiMethod);
     }
 
     @Override
     public void executeAllApiMethods(List<? extends PartialBotApiMethod<Message>> apiMethods) {
         for (PartialBotApiMethod<Message> apiMethod : apiMethods) {
-            Method apiMethodExecutor = methodExecutorResolver.getApiMethodExecutionMethod(apiMethod);
-            ReflectionUtils.invokeMethod(apiMethodExecutor, this, apiMethod);
+            executeApiMethod(apiMethod);
         }
     }
 }
