7eddffe357c2fd4e23e198c7b475342e122979ce
leonid <kotovpetia@gmail.com>
2022-11-24 20:43:42 +0300

    MethodTargetPairContainer update

--- telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/container/MethodTargetPairContainer.java
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/container/MethodTargetPairContainer.java
@@ -2,7 +2,9 @@ package org.telegram.telegrambot.container;
 
 import org.telegram.telegrambot.dto.MethodTargetPair;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,7 +12,24 @@ public abstract class MethodTargetPairContainer<T> {
 
     protected final Map<T, MethodTargetPair> methodTargetPairs = new ConcurrentHashMap<>();
 
+    public Optional<MethodTargetPair> getMethodTargetPair(T key) {
+        MethodTargetPair methodTargetPair = methodTargetPairs.get(key);
+        return Optional.ofNullable(methodTargetPair);
+    }
+
+    public void putMethodTargetPair(T key, MethodTargetPair methodTargetPair) {
+        methodTargetPairs.put(key, methodTargetPair);
+    }
+
     public Set<Map.Entry<T, MethodTargetPair>> getEntrySet() {
         return methodTargetPairs.entrySet();
+    }
+
+    public Set<T> getKeySet() {
+        return methodTargetPairs.keySet();
+    }
+
+    public Collection<MethodTargetPair> getValues() {
+        return methodTargetPairs.values();
     }
 }
