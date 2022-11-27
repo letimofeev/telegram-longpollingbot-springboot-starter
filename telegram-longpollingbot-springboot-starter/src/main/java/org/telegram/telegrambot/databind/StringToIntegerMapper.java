6371f9e53af364877df03e1d6b3c3cee7c42f779
leonid <kotovpetia@gmail.com>
2022-11-27 14:47:15 +0300

    StringToIntegerMapper added

---
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/databind/StringToIntegerMapper.java
@@ -0,0 +1,17 @@
+package org.telegram.telegrambot.databind;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToIntegerMapper implements StringToObjectMapper<Integer> {
+
+    @Override
+    public Integer mapToObject(String value) {
+        return Integer.parseInt(value);
+    }
+
+    @Override
+    public Class<Integer> getType() {
+        return Integer.class;
+    }
+}
