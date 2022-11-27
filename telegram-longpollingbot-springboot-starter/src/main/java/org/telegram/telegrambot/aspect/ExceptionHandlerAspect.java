cf82adf331beb9cb5ea6e31acfee7c40c6ba2434
leonid <kotovpetia@gmail.com>
2022-11-28 01:11:57 +0300

    ExceptionHandlerAspect refactoring

--- telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/aspect/ExceptionHandlerAspect.java
+++ telegram-longpollingbot-springboot-starter/src/main/java/org/telegram/telegrambot/aspect/ExceptionHandlerAspect.java
@@ -10,9 +10,11 @@ import org.telegram.telegrambot.container.ExceptionMappingMethodContainer;
 import org.telegram.telegrambot.dto.InvocationUnit;
 import org.telegram.telegrambot.dto.MethodTargetPair;
 import org.telegram.telegrambot.handler.ApiMethodsReturningMethodInvoker;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.Optional;
+import java.util.List;
 
 @Aspect
 @Component
@@ -39,17 +41,22 @@ public class ExceptionHandlerAspect {
 
     private Object handleException(ProceedingJoinPoint joinPoint, Exception e) throws Exception {
         log.warn("Exception during getResponse() method, nested exception: {}", e.toString());
-        Optional<MethodTargetPair> optional = methodContainer.getMappingForExceptionAssignableFrom(e.getClass());
-        if (optional.isPresent()) {
-            MethodTargetPair methodTargetPair = optional.get();
-            Update update = (Update) joinPoint.getArgs()[0];
-            Object[] args = {update, e};
-            log.warn("Using ExceptionMapping method: {} of class {}", methodTargetPair.getMethod(),
-                    methodTargetPair.getTarget().getClass());
-            return methodInvoker.invokeMethod(new InvocationUnit(methodTargetPair, args));
-        } else {
-            log.error("No handlers found for exception {}", e.getClass());
-            throw e;
-        }
+
+        Class<? extends Exception> exceptionType = e.getClass();
+        return methodContainer.getMappingForExceptionAssignableFrom(exceptionType)
+                .map(exceptionMapping -> invokeExceptionMapping(exceptionMapping, joinPoint, e))
+                .orElseThrow(() -> {
+                    log.error("No handlers found for exception {}", exceptionType.getName());
+                    return e;
+                });
+    }
+
+    private List<? extends PartialBotApiMethod<Message>> invokeExceptionMapping(MethodTargetPair methodTargetPair,
+                                                                                ProceedingJoinPoint joinPoint, Exception e) {
+        Update update = (Update) joinPoint.getArgs()[0];
+        Object[] args = {update, e};
+        log.warn("Using ExceptionMapping method: {} of class {}", methodTargetPair.getMethod(),
+                methodTargetPair.getTarget().getClass());
+        return methodInvoker.invokeMethod(new InvocationUnit(methodTargetPair, args));
     }
 }
